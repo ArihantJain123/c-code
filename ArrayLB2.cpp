b2fd02e4b76c7c2405ab1a2d98c0984c338113fb
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-06-17 13:00:17 +0530

    initial commit

---
+++ ArrayLB2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std ;
+
+int main()
+{
+    int i,n,m;
+
+
+    cout<<"enter the size of an array"<<endl;
+    cin>>m;
+    int arr[m];
+    cout<<" enter array element "<<" "<<endl;
+
+
+    for(i=0; i<m; i++)
+    {
+
+
+        cin>>arr[i];
+    }
+      cout<<"  enter the value of n"<<endl;
+       cin>>n;
+    for(i=0; i<m; i++)
+  {
+    if(arr[i]== n)
+
+        cout<<"TRUE"<<" ";
+
+      if (arr[i]!= n)
+       cout<<"FALSE"<<" ";
+  }
+    return 0;
+
+}
