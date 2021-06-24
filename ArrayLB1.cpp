407ed46530a3a4fa6924c05cc2da81ac9fef33ad
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-06-24 13:01:00 +0530

    new

--- ArrayLB1.cpp
+++ ArrayLB1.cpp
@@ -5,7 +5,7 @@
   {
       int Arr[10],i;
 
-      //for ( int i=0; i<9; i++)
+      //for ( int i=0; i<9; i++) for loop 
         cout<<" enter the value of array element ";
         for ( int i=0; i<10; i++)
         {
